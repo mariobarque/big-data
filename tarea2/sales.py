d9e206ccf1d72559e029c328953b4f8581c27ef5
Mario Alberto Barrantes Quesada <mario@MacBook-Pro.local>
2019-09-22 16:06:57 -0600

    Make it functional

--- tarea2/sales.py
+++ tarea2/sales.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as funcs
 from functools import reduce
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, ArrayType, LongType
-from setuptools import glob
+import pandas as pd
 
 
 def get_json_schema():
@@ -24,7 +24,6 @@ def get_json_schema():
 
 
 def load_data(spark, filename, schema):
-
     sales_df = spark \
         .read \
         .format('json') \
@@ -72,6 +71,76 @@ def get_file_array():
 
     return [file.name for file in args.files]
 
+def get_total_by_products(sales_df):
+    sales_group = sales_df.groupby('producto').agg({'venta': 'sum'})
+    sales_group = sales_group.orderBy(sales_group['sum(venta)'].desc())
+
+    return sales_group.select(funcs.col("producto"),
+                              funcs.col("sum(venta)").alias("ventas"))
+
+def get_total_by_cash_register(sales_df):
+    sales_group = sales_df.groupby('numero_caja').agg({'venta': 'sum'})
+    sales_group = sales_group.orderBy(sales_group['sum(venta)'].desc())
+
+    return sales_group.select(funcs.col("numero_caja"),
+                              funcs.col("sum(venta)").alias("ventas"))
+
+
+def get_cash_register_with_most_sales(sales_df):
+    sales_group = get_total_by_cash_register(sales_df)
+    cr_with_most_sales = sales_group.orderBy(sales_group['ventas'].desc()).first().numero_caja
+
+    return cr_with_most_sales
+
+def get_cash_register_with_least_sales(sales_df):
+    sales_group = get_total_by_cash_register(sales_df)
+    cr_with_most_sales = sales_group.orderBy(sales_group['ventas'].asc()).first().numero_caja
+
+    return cr_with_most_sales
+
+def get_product_with_more_units_sold(sales_df):
+    sales_group = sales_df.groupby('producto').agg({'cantidad': 'sum'})
+    product_most_unit_sold = sales_group.orderBy(sales_group['sum(cantidad)'].desc()).first().producto
+
+    return product_most_unit_sold
+
+def get_product_with_more_sales(sales_df):
+    sales_group = get_total_by_products(sales_df)
+    product_most_unit_sold = sales_group.orderBy(sales_group['ventas'].desc()).first().producto
+
+    return product_most_unit_sold
+
+def get_sales_percentile(sales_df, percentile):
+    total_registers_df = get_total_by_cash_register(sales_df)
+
+    return total_registers_df\
+        .selectExpr('percentile(ventas, ' + str(percentile) + ') as percentile')\
+        .first().percentile
+
+def get_all_metrics(sales_df):
+    data = [
+        ['caja_con_mas_ventas', get_cash_register_with_most_sales(sales_df)],
+        ['caja_con_menos_ventas', get_cash_register_with_least_sales(sales_df)],
+        ['percentil_25_por_caja', get_sales_percentile(sales_df, 0.25)],
+        ['percentil_50_por_caja', get_sales_percentile(sales_df, 0.5)],
+        ['percentil_75_por_caja', get_sales_percentile(sales_df, 0.75)],
+        ['producto_mas_vendido_por_unidad', get_product_with_more_units_sold(sales_df)],
+        ['producto_de_mayor_ingreso', get_product_with_more_sales(sales_df)],
+    ]
+
+    return pd.DataFrame(data)
+
+
+def save_files(sales_df):
+    # total_productos.csv
+    get_total_by_products(sales_df).toPandas().to_csv('total_productos.csv', index=False, header=False)
+
+    # total_cajas.csv
+    get_total_by_cash_register(sales_df).toPandas().to_csv('total_cajas.csv', index=False, header=False)
+
+    # metricas.csv
+    get_all_metrics(sales_df).to_csv('metricas.csv', index=False, header=False)
+
 
 if __name__ == "__main__":
     file_array = get_file_array()
@@ -79,5 +148,6 @@ if __name__ == "__main__":
     spark = SparkSession.builder.appName('database').master('local').getOrCreate()
     sales = union_sales_data(spark, get_json_schema(), file_array)
 
-    print(sales.count())
+    save_files(sales)
+
     sales.show()
---
+++ tarea2/tests/test_sales_total_product.py
@@ -0,0 +1,52 @@
+from tarea2.sales import get_total_by_products
+
+
+def get_schema():
+    return ['numero_caja', 'producto', 'cantidad', 'precio_unitario', 'venta']
+
+def test_get_total_by_products_happy_path(spark_session):
+    sales_data = [(1, 'mango', 1, 50, 50), (1, 'manzana', 2, 10, 10), (1, 'mango', 2, 40, 80)]
+    sales_df = spark_session.createDataFrame(sales_data, get_schema())
+
+    expected = spark_session.createDataFrame([('mango', 130),('manzana', 10)], ['producto', 'ventas'])
+    actual = get_total_by_products(sales_df)
+
+    assert expected.collect() == actual.collect()
+
+
+def test_get_total_by_products_when_only_one_sale(spark_session):
+    sales_data = [(1, 'mango', 1, 50, 50)]
+    sales_df = spark_session.createDataFrame(sales_data, get_schema())
+
+    expected = spark_session.createDataFrame([('mango', 50)], ['producto', 'ventas'])
+    actual = get_total_by_products(sales_df)
+
+    assert expected.collect() == actual.collect()
+
+def test_get_total_by_products_when_only_one_product(spark_session):
+    sales_data = [(1, 'mango', 1, 50, 50), (1, 'mango', 3, 50, 150), (1, 'mango', 3, 50, 150)]
+    sales_df = spark_session.createDataFrame(sales_data, get_schema())
+
+    expected = spark_session.createDataFrame([('mango', 350)], ['producto', 'ventas'])
+    actual = get_total_by_products(sales_df)
+
+    assert expected.collect() == actual.collect()
+
+def test_get_total_by_products_when_quantity_is_zero_should_ignore(spark_session):
+    sales_data = [(1, 'mango', 1, 50, 50), (1, 'manzana', 0, 10, 10), (1, 'mango', 2, 40, 80)]
+    sales_df = spark_session.createDataFrame(sales_data, get_schema())
+
+    expected = spark_session.createDataFrame([('mango', 130)], ['producto', 'ventas'])
+    actual = get_total_by_products(sales_df)
+
+    assert expected.collect() == actual.collect()
+
+def test_get_total_by_products_when_quantity_is_negative_should_ignore(spark_session):
+    sales_data = [(1, 'mango', 1, 50, 50), (1, 'manzana', -1, 10, 10), (1, 'mango', 2, 40, 80)]
+    sales_df = spark_session.createDataFrame(sales_data, get_schema())
+
+    expected = spark_session.createDataFrame([('mango', 130)], ['producto', 'ventas'])
+    actual = get_total_by_products(sales_df)
+
+    assert expected.collect() == actual.collect()
+
